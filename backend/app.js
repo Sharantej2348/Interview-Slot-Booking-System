01f700bf67e6773044807fbbd9846be2e0065741
Sharantej2348 <sharantej7623@gmail.com>
2026-02-17 14:06:39 +0530

    Enhance CORS configuration in app.js and ensure axios requests include credentials

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,15 @@ import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://interview-slot-booking-system-front.vercel.app",
+        ],
+        credentials: true,
+    }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
